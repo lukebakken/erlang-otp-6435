df7e05dd7d26fb1b3ec0765ebc8af5a7c99be590
Luke Bakken <luke@bakken.io>
2022-11-14 13:41:59 -0800

    Tweak

--- run-tls-server.sh
+++ run-tls-server.sh
@@ -5,4 +5,4 @@ readonly curdir="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
 readonly logger_level="${1:-error}"
 readonly tls_module="${2:-tls_server}"
 erlc +debug "$curdir/src/$tls_module.erl"
-erl -noinput -kernel logger_level "$logger_level" -s tls_server start
+erl -noinput -kernel logger_level "$logger_level" -s "$tls_module" start
